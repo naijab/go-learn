34e812317fb9e2ad5a9bdb228ac53f3492d38320
Nattapon Pondongnok <nainatjab999@gmail.com>
2025-05-09 15:05:38 +0700

    feat: add palindrome

---
+++ palindrome/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func longestPalindrome(s string) string {
+	if len(s) <= 1 {
+		return s
+	}
+
+	result := ""
+
+	for i := 0; i < len(s); i++ {
+		// Check for odd-length palindrome centered at i
+		odd := expandAroundCenter(s, i, i)
+		fmt.Println("odd: ", odd)
+
+		// Check for even-length palindrome centered between i and i+1
+		even := expandAroundCenter(s, i, i+1)
+		fmt.Println("even: ", even)
+
+		// Update longest if a longer palindrome is found
+		if len(odd) > len(result) {
+			result = odd
+		}
+
+		if len(even) > len(result) {
+			result = even
+		}
+	}
+
+	return result
+}
+
+func expandAroundCenter(s string, left, right int) string {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		left--
+		right++
+	}
+
+	fmt.Println("left: ", left)
+	fmt.Println("right: ", right)
+	return s[left+1 : right]
+}
+
+func main() {
+	input := "xabaz"
+	output := longestPalindrome(input)
+	fmt.Println("input : ", input)
+	fmt.Println("output : ", output)
+}
